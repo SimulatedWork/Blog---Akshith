1e8cee11f8f8a8244fae69a245f1e9a7f3cf6971
akshithku <akshithkumar.karla@gmail.com>
2023-11-22 16:38:38 +0530

    Backend and Frontend

--- frontend/src/components/viewPage.js
+++ frontend/src/components/viewPage.js
@@ -3,8 +3,10 @@
 import React, { useState, useEffect } from "react";
 import "./viewPage.css";
 import { useNavigate, useParams } from "react-router-dom";
+import { HeartTwoTone } from "@ant-design/icons";
+import { Modal, Input, Button, Comments, message } from "antd";
+import { CommentOutlined } from '@ant-design/icons';
 
-import { Modal, Input, Button,  Comments  } from "antd";
 import { jwtDecode } from "jwt-decode";
 
 export default function ViewPage() {
@@ -12,11 +14,15 @@ export default function ViewPage() {
   const [viewdata, setViewdata] = useState([]);
   const [bloglike, setBloglike] = useState([]);
   const navigate = useNavigate();
-  const [ viewlike,setViewlike] = useState("");
+  const [viewlike, setViewlike] = useState("");
   const [isCommentModalVisible, setIsCommentModalVisible] = useState(false);
-  
+
   const [comment, setComment] = useState("");
   const [comments, setComments] = useState([]);
+
+  const [showComments, setShowComments] = useState([]);
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   let Username;
   let userId;
@@ -25,20 +31,22 @@ export default function ViewPage() {
     console.log("View data", viewdata);
   }, [viewdata]);
   useEffect(() => {
-    const token = localStorage.getItem('token');
-    if(token){
-      const UserToken=localStorage.getItem("token");
-      console.log("User_Token :",UserToken)
-      const decodedToken = jwtDecode(token );
-      console.log("decoded:",decodedToken)
-       userId = decodedToken._id;
-    console.log("userid",userId);
-    
-       Username= decodedToken.name;
-      console.log("Username:",Username);
+    const token = localStorage.getItem("token");
+    if (token) {
+      const UserToken = localStorage.getItem("token");
+      console.log("User_Token :", UserToken);
+      const decodedToken = jwtDecode(token);
+      console.log("decoded:", decodedToken);
+      userId = decodedToken._id;
+      console.log("userid", userId);
+
+      Username = decodedToken.name;
+      console.log("Username:", Username);
       fetch(`http://localhost:2004/blogview/${id}`, {
         headers: {
           Authorization: `Bearer ${UserToken}`,
+          //   name:`${Username}`,
+          // email:`${UserToken.email}`
         },
       })
         .then((response) => response.json())
@@ -49,39 +57,37 @@ export default function ViewPage() {
         .catch((error) => {
           console.log(error, " failed to fetch");
         });
-    }else{
-      navigate('/loginPage')
+    } else {
+      navigate("/loginPage");
     }
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  
 
   // Inside your BlogPage component
   const handleLikeClick = (id) => {
-    const token = localStorage.getItem('token');
-    if(token){
-    fetch(`http://localhost:2004/like/${id}`, {
-      method: "PUT",
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${token}`,
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        setBloglike(data);
-        console.log("Data", data);
+    const token = localStorage.getItem("token");
+    if (token) {
+      fetch(`http://localhost:2004/like/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
       })
-      .catch((error) => {
-        console.error("Failed to like the blog post:", error);
-      });
-    }else{
+        .then((response) => response.json())
+        .then((data) => {
+          setBloglike(data);
+          console.log("Data", data);
+        })
+        .catch((error) => {
+          console.error("Failed to like the blog post:", error);
+        });
+    } else {
       console.log("error");
     }
   };
 
-  
   useEffect(() => {
     console.log("view likedata", viewlike);
   }, [viewlike]);
@@ -98,41 +104,71 @@ export default function ViewPage() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+ 
+  useEffect(() => {
+    fetch(`http://localhost:2004/displayComments/${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Comments data", data);
+        setShowComments(data);
+      })
+      .catch((error) => {
+        console.log(error, " failed to fetch");
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [id]);
 
-  const Commentsubmit= async()=>{
+
+  const showCommentsModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleCommentsOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCommentsCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  const Commentsubmit = async () => {
     // e.preventDefault();
-   
+    const token = localStorage.getItem("token");
+    if (token) {
+      const decodedToken = jwtDecode(token);
+      console.log("decoded:", decodedToken);
+      userId = decodedToken._id;
+      console.log("userid", userId);
+
+      Username = decodedToken.name;
+      console.log("Username:", Username);
       const submit = await fetch(`http://localhost:2004/addComment/${id}`, {
         method: "PUT",
-  
+
         headers: {
           "Content-type": "application/json; charset=UTF-8",
         },
-  
+
         body: JSON.stringify({
-          "userId":userId,
-          "text":comment,
-          "username":Username,
+          userId: userId,
+          text: comment,
+          username: Username,
         }),
-  
       })
         .then((res) => res.json())
         .then((json) => {
           console.log(json);
+          message.success("Thanks for commenting");
         })
         .catch((err) => {
           console.log(err);
+          message.error("Something went Wrong");
         });
-  
-        console.log("BLOG DATA :",submit)
-    
-    
-      // console.log("Bolg Image",BlogIMG);
-  
-  }
-  
- 
 
+      // console.log("BLOG Comment :", submit);
+    } else {
+      console.log("Failed to comment");
+    }
+    // console.log("Bolg Image",BlogIMG);
+  };
 
   const showModal = () => {
     setIsCommentModalVisible(true);
@@ -141,7 +177,7 @@ export default function ViewPage() {
   const handleOk = () => {
     setComments([...comments, { author: "User", content: comment }]);
     setIsCommentModalVisible(false);
-    setComment(""); 
+    setComment("");
     Commentsubmit();
   };
 
@@ -151,28 +187,68 @@ export default function ViewPage() {
 
   return (
     <div className="viewPage-main">
-      <div className="viewPage-sub1">
-        <div className="img-div">
-          <img className="viewPage-img" src={viewdata.Blogimg} alt="img" />
-        </div>
-        <div className="viewPage-divimg">
-          <div className="viewpage-label">
-            <h1 style={{ margin: "0" }} className="ViewPage-h1">
-              {viewdata.BlogName}
-              {" :-"}
-            </h1>
+      <div className="viewPage-main-2">
+        <div className="viewPage-sub1">
+          <div className="img-div">
+            <img className="viewPage-img" src={viewdata.Blogimg} alt="img" />
           </div>
-          <p className="ViewPage-para">{viewdata.BlogContent}</p>
+          <div className="viewPage-divimg">
+            <div className="ViewPage-likes">
+              <div className="ViePage-like-view">
+                {/* <p> */}
+                  <HeartTwoTone twoToneColor="#eb2f96" style={{ fontSize: '4vmin' }} />
+                  {viewlike.BlogLike}
+                {/* </p> */}
+              </div>
+              <div className="ViewPage-comments-view">
+                <p className="Viewpage-btn3" onClick={showCommentsModal}><CommentOutlined style={{ fontSize: '5vmin', color: 'black', cursor:'pointer' }}/></p>
+                <Modal
+                  title="Comments"
+                  open={isModalOpen}
+                  onOk={handleCommentsOk}
+                  onCancel={handleCommentsCancel}
+                  footer={null}
+                >
+                 {
+                  showComments.BlogComments?.length > 0 ? (
+                    showComments.BlogComments
+                    .map((data)=>(
+                      <div>
+                      <h1>{data.username}</h1>
+                      <p>{data.text}</p>
+                      </div>
+                    ))
+                  ):(
+                    <div>There are no Comments !!!</div>
+                  )
+                 }
+                </Modal>
+              </div>
+            </div>
+            <div className="viewpage-label">
+              <h1 style={{ margin: "0" }} className="ViewPage-h1">
+                {viewdata.BlogName}
+                {" :-"}
+              </h1>
+            </div>
+            <p className="ViewPage-para">{viewdata.BlogContent}</p>
+          </div>
         </div>
+        {/* <div>
+        <p>{viewlike.BlogLike}</p>
+      </div> */}
       </div>
       <div className="ViewPage-btn-div">
         <div className="ViewPage-like">
           <button className="like-btn" onClick={() => handleLikeClick(id)}>
-            Like{viewlike.BlogLike}
+            Like
+            {/* {viewlike.BlogLike} */}
           </button>
         </div>
         <div className="viewPage-comment">
-          <button className="comment-btn" onClick={showModal}>Comment</button>
+          <button className="comment-btn" onClick={showModal}>
+            Comment
+          </button>
         </div>
       </div>
       <Modal
@@ -191,10 +267,10 @@ export default function ViewPage() {
           className="commentStyle"
         />
         {/* {comments.map((c, index) => ( */}
-          {/* <div> */}
-          {/* <Comments key={index} author={c.author} content={c.content}  className="commentStyle" /> */}
-          {/* <p>Hi</p> */}
-          {/* </div> */}
+        {/* <div> */}
+        {/* <Comments key={index} author={c.author} content={c.content}  className="commentStyle" /> */}
+        {/* <p>Hi</p> */}
+        {/* </div> */}
         {/* ))} */}
       </Modal>
     </div>
